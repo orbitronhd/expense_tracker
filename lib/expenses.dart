8d37f0e79743ca14704bc842b85d50f674cded9a
Jonathan Jimson <orbitron.hd.official@gmail.com>
2026-05-21 13:26:44 +0530

    Add dummy values

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
+  const Expenses({super.key});
 
-    const Expenses({super.key});
-
-    @override
+  @override
   State<StatefulWidget> createState() {
     return _ExpensesState();
   }
 }
 
 class _ExpensesState extends State<Expenses> {
-    @override
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 29.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-            children: [
-                Text("Chart")
-            ],
-        )
-    );
+    return Scaffold(body: Column(children: [Text("Chart")]));
   }
 }
