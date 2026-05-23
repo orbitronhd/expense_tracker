b60413c77d8b94c4bada4075dd21700d63c21411
Jonathan Jimson <orbitron.hd.official@gmail.com>
2026-05-24 00:00:10 +0530

    Add intl package and update imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:expense_tracker/expenses.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Expenses(),
-    ),
-  );
+  runApp(const MaterialApp(home: Expenses()));
 }
